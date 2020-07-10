4d0f6c0ec724e1249b62bbbae77ff5244da10eec
Robert Broersma <robertbroersma@gmail.com>
2020-07-10 14:54:28 +0200

    Random seed for svg function

--- site/functions/svg/svg.js
+++ site/functions/svg/svg.js
@@ -1,12 +1,85 @@
 const React = require('react')
 const RDS = require('react-dom/server')
-const { BigHead } = require('@bigheads/core')
+const seedrandom = require('seedrandom')
+
+const {
+  BigHead,
+  theme,
+  eyesMap,
+  eyebrowsMap,
+  mouthsMap,
+  hairMap,
+  facialHairMap,
+  clothingMap,
+  accessoryMap,
+  graphicsMap,
+  hatMap,
+  bodyMap,
+} = require('@bigheads/core')
+
+function getRandomOptions(rng) {
+  function selectRandomKey(object) {
+    return Object.keys(object)[Math.floor(rng() * Object.keys(object).length)]
+  }
+
+  const skinTone = selectRandomKey(theme.colors.skin)
+  const eyes = selectRandomKey(eyesMap)
+  const eyebrows = selectRandomKey(eyebrowsMap)
+  const mouth = selectRandomKey(mouthsMap)
+  const hair = selectRandomKey(hairMap)
+  const facialHair = selectRandomKey(facialHairMap)
+  const clothing = selectRandomKey(clothingMap)
+  const accessory = selectRandomKey(accessoryMap)
+  const graphic = selectRandomKey(graphicsMap)
+  const hat = selectRandomKey(hatMap)
+  const body = selectRandomKey(bodyMap)
+
+  const hairColor = selectRandomKey(theme.colors.hair)
+  const clothingColor = selectRandomKey(theme.colors.clothing)
+  const circleColor = selectRandomKey(theme.colors.bgColors)
+  const lipColor = selectRandomKey(theme.colors.lipColors)
+  const hatColor = selectRandomKey(theme.colors.clothing)
+
+  const mask = true
+  const lashes = rng() > 0.5
+
+  return {
+    skinTone,
+    eyes,
+    eyebrows,
+    mouth,
+    hair,
+    facialHair,
+    clothing,
+    accessory,
+    graphic,
+    hat,
+    body,
+    hairColor,
+    clothingColor,
+    circleColor,
+    lipColor,
+    hatColor,
+    mask,
+    lashes,
+  }
+}
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async event => {
   try {
-    const props = event.queryStringParameters
-    const avatarString = RDS.renderToString(React.createElement(BigHead, props))
+    const { seed, ...props } = event.queryStringParameters
+
+    const rng = seed ? seedrandom(seed) : Math.random
+
+    const mergedProps = {
+      ...getRandomOptions(rng),
+      ...props,
+    }
+
+    const avatarString = RDS.renderToString(
+      React.createElement(BigHead, mergedProps),
+    )
 
     return {
       statusCode: 200,
