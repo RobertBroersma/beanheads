4368233fe4f437a150570726f549d25de8864146
Adam Sobotka <vorcigernix@gmail.com>
2021-08-29 19:40:39 +0200

    Create Heart.tsx
    I'd need a symbol of heart for my project.

---
+++ core/src/components/clothingGraphic/Heart.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const HeartGraphic = () => {
+  return (
+    <>
+         <path
+            fillRule="evenodd"
+            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+            clipRule="evenodd"
+            fill="#e73c7e"
+          />
+    </>
+  )
+}
